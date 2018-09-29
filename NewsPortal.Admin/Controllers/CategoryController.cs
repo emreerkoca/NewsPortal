db18be3c586ad03e8176f968a92703e8d73ce41a
Emre Erkoca <emreerkoca@outlook.com>
2018-09-30 00:59:09 +0300

    Some operations
    Using PagedList, Update and Delete operations

--- NewsPortal.Admin/Controllers/CategoryController.cs
+++ NewsPortal.Admin/Controllers/CategoryController.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
+using NewsPortal.Admin.CustomFilter;
 
 namespace NewsPortal.Admin.Controllers
 {
@@ -20,10 +22,12 @@ namespace NewsPortal.Admin.Controllers
         }
         #endregion
 
-        public ActionResult Index()
+        #region List Category
+        public ActionResult Index(int page = 1)
         {
-            return View(_categoryRepository.GetAll().ToList());
-        }
+            return View(_categoryRepository.GetAll().OrderByDescending(x => x.ID).ToPagedList(page, 10));
+        } 
+        #endregion
 
         #region Insert Category
         [HttpGet]
@@ -58,17 +62,49 @@ namespace NewsPortal.Admin.Controllers
         #endregion
 
         #region Delete Category
-        public ActionResult Delete(int ID)
+        public JsonResult Delete(int ID)
         {
             Category deleteCategory = _categoryRepository.GetById(ID);
             if(deleteCategory == null)
             {
-                throw new Exception("Category not found");
+                return Json(new ResultJson { Success = false, Message = "Category not found!" });
             }
             _categoryRepository.Delete(ID);
             _categoryRepository.Save();
 
-            return RedirectToAction("Index", "Category");
+            return Json(new ResultJson { Success = true, Message = "Delete category operation is successful!" });
+        }
+        #endregion
+
+        #region Update Category
+        [HttpGet]
+        [LoginFilter]
+        public ActionResult Update(int id)
+        {
+            Category dbCategory = _categoryRepository.GetById(id);
+            if(dbCategory == null)
+            {
+                throw new Exception("Category not found");
+            }
+            SetCategoryList();
+            return View(dbCategory);
+        }
+
+        [HttpPost]
+        [LoginFilter]
+        public JsonResult Update(Category category)
+        {
+            //if(ModelState.IsValid)
+            //{
+                Category dbCategory = _categoryRepository.GetById(category.ID);
+                dbCategory.IsActive = category.IsActive;
+                dbCategory.CategoryName = category.CategoryName;
+                dbCategory.ParentID = category.ParentID;
+                dbCategory.URL = category.URL;
+                _categoryRepository.Save();
+                return Json(new ResultJson { Success = true, Message = "Update category operation successful!" });
+            //}
+            //return Json(new ResultJson { Success = false, Message = "An error occured. Update category operation unsuccessful!" });
         }
         #endregion
     }
--- NewsPortal.Admin/Scripts/Global.js
+++ NewsPortal.Admin/Scripts/Global.js
@@ -10,9 +10,8 @@
         url: "Insert",
         data: category,
         type: "POST",
-        dataType: 'json',
+        dataType: "json",
         success: function (response) {
-            debugger;
             if (response.Success) {
                 bootbox.alert(response.Message, function () {
                     location.reload();
@@ -26,4 +25,54 @@
         }
     })
 
+}
+
+function DeleteCategory() {
+    var deleteID = $("#btnDeleteCategory").attr("data-id");
+
+    $.ajax({
+        url: "Delete/" + deleteID,
+        type: "POST",
+        dataType: "json",
+        success: function (response) {
+            if (response.Success) {
+                bootbox.alert(response.Message, function () {
+                    location.reload();
+                });
+            }
+            else {
+                bootbox.alert(response.Message, function () {
+                    //TODO
+                });
+            }
+        }
+    });
+}
+
+function UpdateCategory() {
+    category = new Object();
+    category.CategoryName = $("#categoryName").val();
+    category.Url = $("#categoryUrl").val();
+    category.IsActive = $("#isActiveCategory").is(":checked");
+    category.ParentID = $("#ParentID").val();
+    category.ID = $("#ID").val();
+
+    $.ajax({
+        url: "Update",
+        data: category,
+        type: "POST",
+        dataType: "json",
+        success: function (response) {
+            if (response.Success) {
+                bootbox.alert(response.Message, function () {
+                    location.reload();
+                });
+            }
+            else {
+                bootbox.alert(response.Message, function () {
+                    //TODO
+                });
+            }
+        }
+    })
 }
