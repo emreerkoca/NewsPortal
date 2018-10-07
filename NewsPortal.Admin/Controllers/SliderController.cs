d57d2fab43714174523c5f160a5b5209e78ebb93
Emre Erkoca <emreerkoca@outlook.com>
2018-10-07 03:26:55 +0300

    Delete Sliders  and Image Upload
    Delete Sliders  and Image Upload

--- NewsPortal.Admin/Controllers/SliderController.cs
+++ NewsPortal.Admin/Controllers/SliderController.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using PagedList;
+using NewsPortal.Admin.Helper;
+using System.IO;
 
 namespace NewsPortal.Admin.Controllers
 {
@@ -28,6 +30,7 @@ namespace NewsPortal.Admin.Controllers
             return View(slider);
         }
 
+        #region Insert
         [LoginFilter]
         [HttpGet]
         public ActionResult Insert()
@@ -39,9 +42,9 @@ namespace NewsPortal.Admin.Controllers
         [HttpPost]
         public JsonResult Insert(Slider slider, HttpPostedFileBase imageURL)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                if(imageURL.ContentLength > 0)
+                if (imageURL.ContentLength > 0)
                 {
                     string file = Guid.NewGuid().ToString().Replace("-", "");
                     string ext = System.IO.Path.GetExtension(Request.Files[0].FileName);
@@ -64,5 +67,94 @@ namespace NewsPortal.Admin.Controllers
 
             return Json(new ResultJson { Success = false, Message = "An error occured add slider process!" });
         }
+        #endregion
+
+        #region Edit
+        [HttpGet]
+        [LoginFilter]
+        public ActionResult Edit(int ID)
+        {
+            var slider = _sliderRepository.GetById(ID);
+            if (slider != null)
+            {
+                return View(slider);
+            }
+            return View();
+        } 
+ 
+        [HttpPost]
+        [LoginFilter]
+        public ActionResult Edit(Slider slider, HttpPostedFileBase imageURL)
+        {
+            if(ModelState.IsValid)
+            {
+                Slider dbSlider = _sliderRepository.GetById(slider.ID);
+                dbSlider.IsActive = slider.IsActive;
+                dbSlider.Title = slider.Title;
+                dbSlider.Description = slider.Description;
+                dbSlider.UploadDate = slider.UploadDate;
+                dbSlider.Url = slider.Url;
+                if(imageURL != null && imageURL.ContentLength > 0)
+                {
+                    if(dbSlider.ImageURL != null)
+                    {
+                        string URL = dbSlider.ImageURL;
+                        string imagePath = Server.MapPath(URL);
+                        FileInfo files = new FileInfo(imagePath);
+                        if(files.Exists)
+                        {
+                            files.Delete();
+                        }
+                    }
+
+                    ImageUpload.Image(imageURL, slider);
+                    dbSlider.ImageURL = slider.ImageURL;
+                }
+
+                try
+                {
+                    _sliderRepository.Save();
+                    return Json(new ResultJson { Success = true, Message = "Slider edit operation successful" });
+                }
+                catch (Exception)
+                {
+                    return Json(new ResultJson { Success = false, Message = "An error occured while slider edit operation" });
+                }
+            }
+            return Json(new ResultJson { Success = false, Message = "An error occured while slider edit operation" });
+        }
+        #endregion
+
+        #region Delete
+        public JsonResult Delete(Slider slider)
+        {
+            var dbSlider = _sliderRepository.GetById(slider.ID);
+            if(dbSlider == null)
+            {
+                return Json(new ResultJson { Success = false, Message = "Slider not found." });
+            }
+
+            try
+            {
+                if (dbSlider.ImageURL != null)
+                {
+                    string URL = dbSlider.ImageURL;
+                    string imagePath = Server.MapPath(URL);
+                    FileInfo files = new FileInfo(imagePath);
+                    if (files.Exists)
+                    {
+                        files.Delete();
+                    }
+                }
+                _sliderRepository.Delete(slider.ID);
+                _sliderRepository.Save();
+                return Json(new ResultJson { Success = true, Message = "Slider deleted." });
+            }
+            catch (Exception)
+            {
+                return Json(new ResultJson { Success = false, Message = "An error occured while deleting slider!" });
+            }
+        }
+        #endregion
     }
 }
---
+++ NewsPortal.Admin/Helper/ImageUpload.cs
@@ -0,0 +1,23 @@
+﻿using NewsPortal.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsPortal.Admin.Helper
+{
+    public static class ImageUpload
+    {
+        public static string Image(HttpPostedFileBase imageURL, Slider slider)
+        {
+            string fileName = new Guid().ToString().Replace("-", "");
+            string[] exts = imageURL.ContentType.Split('/');
+            string fullPath = "/External/Slider/" + fileName + exts[1]; //exts[1] = file extension  
+
+            imageURL.SaveAs(System.Web.HttpContext.Current.Server.MapPath(fullPath));
+            slider.ImageURL = fullPath;
+
+            return slider.ImageURL;
+        }
+    }
+}
